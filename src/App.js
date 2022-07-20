d252793059f1506dfe50d983734cc75433fa64c2
Teddy N'go <teddy@teamtangible.com>
2022-07-20 00:18:22 -0400

    Started styling

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+const App = () => {
+
+
+    return (
+        <>
+            <div className="header">Pathfinding Visualizer</div>
+            <div className="navbar">
+                <div className="navbar-inner">
+                    <span className="option">Algos</span>
+                    <span className="option">Maze Patterns</span>
+                    <span className="option">Clear Board</span>
+                    <span className="option">Clear Path</span>
+                    <span className="option">Speed</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
+// import './styles.css';
 import Board from "./Board";
+import './index.css';
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Board />
+    {/*<Board />*/}
+    <App />
   </React.StrictMode>
 );
