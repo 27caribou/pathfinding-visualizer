8bc3b78181aa28274d361ad2b1484af1ff227ebc
Teddy N'go <teddy@teamtangible.com>
2022-07-21 00:05:44 -0400

    More styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 
 const App = () => {
 
+    const navigation = <section className="navigation">
+        <div className="header">Pathfinding Visualizer</div>
+        <div className="navbar">
+            <div className="navbar-inner">
+                <span className="option">Algorithms</span>
+                <span className="option">Maze Patterns</span>
+                <span className="option">Clear Board</span>
+                <span className="option">Clear Path</span>
+                <span className="option">Speed</span>
+            </div>
+        </div>
+    </section>
 
     return (
         <>
-            <div className="header">Pathfinding Visualizer</div>
-            <div className="navbar">
-                <div className="navbar-inner">
-                    <span className="option">Algos</span>
-                    <span className="option">Maze Patterns</span>
-                    <span className="option">Clear Board</span>
-                    <span className="option">Clear Path</span>
-                    <span className="option">Speed</span>
+            { navigation }
+            <section className="control-board">
+                <div>
+                    <div className="legend">
+                        <h3>Legend</h3>
+                        <ul className="item-list">
+                            { ['Start', 'Target', 'Weight', 'Wall', 'Visited', 'Shortest-path'].map( x => {
+                                return  (
+                                    <li className="item">
+                                        <div className="desc">
+                                            <span className={ x.toLowerCase() + " symbol" }></span>
+                                            {x} Node
+                                        </div>
+                                    </li>
+                                )
+                            }) }
+                        </ul>
+                    </div>
+                    <div>
+                        <button className="find-path">Find Path!</button>
+                    </div>
                 </div>
-            </div>
+                <p>Pick an algorithm!</p>
+            </section>
         </>
     )
 }
